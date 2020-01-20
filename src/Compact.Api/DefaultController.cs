43de38d77fb5461451de8ff56fee92163855f55e
Tom McClean <spam@tommcclean.me>
2020-01-20 11:00:51 +0000

    Redirect root requests to swagger (#3)
    * Redirect root requests to swagger
    
    * Exclude default action from swagger docs

---
+++ src/Compact.Api/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Compact.Api
+{
+    public class DefaultController : Controller
+    {
+        [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ActionResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
