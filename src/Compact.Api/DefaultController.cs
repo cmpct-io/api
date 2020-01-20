2fce4b419bc6ff85f13691da0bb86f9c98e48696
Thomas William McClean <spam@tommcclean.me>
2020-01-20 10:57:56 +0000

    Exclude default action from swagger docs

--- src/Compact.Api/DefaultController.cs
+++ src/Compact.Api/DefaultController.cs
@@ -5,6 +5,7 @@ namespace Compact.Api
     public class DefaultController : Controller
     {
         [Route(""), HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
         public ActionResult RedirectToSwaggerUi()
         {
             return Redirect("/swagger");
