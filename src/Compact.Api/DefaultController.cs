666480d9aae843d53f0ebbe844445c8f20c113e6
Thomas William McClean <spam@tommcclean.me>
2020-01-20 10:56:12 +0000

    Redirect root requests to swagger

---
+++ src/Compact.Api/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Compact.Api
+{
+    public class DefaultController : Controller
+    {
+        [Route(""), HttpGet]
+        public ActionResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
